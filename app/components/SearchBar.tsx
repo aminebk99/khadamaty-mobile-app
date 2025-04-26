7d41bbdcbeda29b3d9f6fcef7f4596836a1828e0
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2025-04-26 09:48:25 +0100

    make the search bar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -1,13 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { useLocalSearchParams, usePathname } from 'expo-router'
 
 const SearchBar = () => {
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query?: string }>();
+  const [search, setSearch] = useState(params.query);
+
+  const handleSearch = (text: string) => {
+    setSearch(text);
+  }
   return (
-    
-      <View className="justify-center p-4">
-        <Text>Search Bar Components</Text>
+
+    <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2">
+      <View className='flex-1 flex flex-row items-center'>
+        <TextInput
+          value={search}
+          onChangeText={handleSearch}
+          placeholder='Search for anything'
+          className='text-sm ml-2 flex-1'
+        />
       </View>
-    
+    </View>
+
   )
 }
 
